d253f282b5f066cdfa5fcd396278fd29f48ac1f2
cac81 <grupocac8@gmail.com>
2023-10-16 09:56:55 -0300

    agregado del txt

--- proyecto/javascript/js-turnos.js
+++ proyecto/javascript/js-turnos.js
@@ -1,11 +1,2 @@
-document.getElementById("horario-1").addEventListener("active", reservaTurno("1"));
-document.getElementById("horario-2").addEventListener("click", reservaTurno);
-document.getElementById("horario-3").addEventListener("click", reservaTurno);
 
-
-function reservaTurno(numero){
-
-window.alert("click en " + numero + "dia lunes");
-
-}
 
