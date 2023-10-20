9dac929b2ad620abe87d1072820a841d66694f62
cac81 <grupocac8@gmail.com>
2023-10-20 20:04:58 -0300

    para la pagina

---
+++ javascript/js-validacion-medicos.js
@@ -0,0 +1,33 @@
+
+function validarFormulario(usuario, contraseña)
+{
+    var usuario= document.getElementById("usuario").value.trim();
+    var contraseña = document.getElementById("contraseña").value.trim();
+        
+    if( usuario ==='' || contraseña ==='')
+            {
+    
+            window.alert('debe ingresar todos los campos solicitados');
+                return false;
+        
+             }
+        
+         for (var i = 0; i < usuario.length; i++) {
+            var charCode = usuario.charCodeAt(i);
+            if (!((charCode >= 65 && charCode <= 90) || (charCode >= 97 && charCode <= 122) )) {
+              alert("El campo usuario solo puede contener caracteres alfabéticos.");
+              return false;
+                }
+              
+
+              /*regex /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}$/*/
+          
+              
+              alert("Ingreso al sistema");
+              return true;
+            }
+
+
+
+
+}
